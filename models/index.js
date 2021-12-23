f05eb815d0f6e17832e4a10c1a68cfcf1cda1035
Sloan <Sloan.boyce@gmail.com>
2021-12-22 19:50:10 -0500

    voted posts now are displayed when a user is being searched for

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User')
 const Post = require('./Post')
+const Vote = require('./Vote')
 
 
 // create associations 
@@ -11,5 +12,33 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
+// instruct user and post models that they will be connected but through vote which aligns with the fields set up in the Vote model
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+})
 
-module.exports = { User, Post }
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+})
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+})
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+})
+
+module.exports = { User, Post, Vote }
